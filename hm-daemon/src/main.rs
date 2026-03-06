74f43d3a6c34cb4efe06e24733f158491777dcf7
Ayaan Mirza Baig <ayaanmirzabaig85@gmail.com>
2026-03-06 00:26:04 +0000

    feat: implement thread-safe CommandRegistry state using Arc and RwLock

--- hm-daemon/src/main.rs
+++ hm-daemon/src/main.rs
@@ -1,13 +1,17 @@
 mod models;
+mod state;
 
+use crate::state::new_shared_state;
 use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    // Init foundational router for monitoring data
-    let app = Router::new().route("/status", get(|| async { "Daemon Operational" }));
+    let app_state = new_shared_state();
 
-    // Bind to all nics on port 3000
+    let app = Router::new()
+        .route("/status", get(|| async { "Daemon Operational" }))
+        .with_state(app_state);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("Server is listening on: {}", listener.local_addr().unwrap());
 
