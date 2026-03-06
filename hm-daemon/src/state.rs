65df1e7efe06939096414966d50ae787d7e741d8
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2026-03-06 06:06:56 +0530

    fix: properly fix schema

--- hm-daemon/src/state.rs
+++ hm-daemon/src/state.rs
@@ -4,19 +4,20 @@ use r2d2_sqlite::SqliteConnectionManager;
 pub type DbPool = Pool<SqliteConnectionManager>;
 
 pub fn init_db_pool() -> DbPool {
-    let manager = SqliteConnectionManager::file("helmsman.db");
-    let pool = Pool::new(manager).expect("Failed to initialize SQLite connection pool");
-    let conn = pool
-        .get()
-        .expect("Failed to get SQLite connection from pool");
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS commands(
+	let manager = SqliteConnectionManager::file("helmsman.db");
+	let pool = Pool::new(manager).expect("Failed to initialize SQLite connection pool");
+	let conn = pool
+		.get()
+		.expect("Failed to get SQLite connection from pool");
+	conn.execute(
+		"CREATE TABLE IF NOT EXISTS commands(
 		id TEXT PRIMARY KEY,
 		name TEXT NOT NULL,
+		command TEXT NOT NULL,
 		working_dir TEXT,
 		timeout_seconds INTEGER)",
-        (),
-    )
-    .expect("Failed to execute schema DDL");
-    pool
+		(),
+	)
+		.expect("Failed to execute schema DDL");
+	pool
 }
